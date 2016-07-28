ac1e14411b39e691fccce18564126246eed0bc08
PriestofTuna <lyttekk@yahoo.com>
2016-07-28 17:08:35 -0400

    Nyah

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,12 +7,13 @@ import javafx.scene.control.TextArea;
 import java.util.ArrayList;
 
 public class Controller {
-    ArrayList<String> stList = new ArrayList<>();
-    ArrayList<Integer> inList = new ArrayList<>();
+    private ArrayList<String> stList = new ArrayList<>();
+    private ArrayList<Integer> inList = new ArrayList<>();
     //Initializes the lists
     @FXML   TextArea output;
     @FXML
     public void getList(ActionEvent actionEvent) {
+        output.deleteText(0, output.getLength());
         String first ="first", second = "second", third = "Third", fourth = "fourth", fifth = "fifth", sixth = "sixth";
         stList.add(first);
         stList.add(second);
@@ -22,19 +23,32 @@ public class Controller {
         stList.add(sixth);
         //creates dummy values
         String send ="";
+        String sendInt="";
+        int firstI=1, secondI=2, thirdI=3, fourthI=4, fifthI=5, sixthI =6;
+        inList.add(firstI);
+        inList.add(secondI);
+        inList.add(thirdI);
+        inList.add(fourthI);
+        inList.add(fifthI);
+        inList.add(sixthI);
+        //dummy values
         shuffle(stList);
+        shuffle(inList);
         //calls shuffle
+
         for(int i = 0; i < stList.size(); i++) {
-            send += stList.get(i);
+            send += stList.get(i) + " ";
             //makes a String hold the values, whether they be objects or values
         }
-        output.setText(send);
+        for(int i = 0; i < inList.size(); i++) {
+            sendInt += "\t" +inList.get(i);
+        }
+        output.setText("\n" +send+ "\n" + sendInt);
         //outputs the text
     }
 
-    public static <E> void shuffle(ArrayList<E> list) {
+    private static <E> void shuffle(ArrayList<E> list) {
         int rando;
-
         ArrayList<E> nyah = new ArrayList<>();
         for(int i = 0; i < list.size(); i++) {
             rando = (int)(Math.random() * (list.size()));
